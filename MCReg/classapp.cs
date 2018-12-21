6d0972d5fc3894faeb7172d63ebd3f487ed946fc
mpadula1990 <44302654+mpadula1990@users.noreply.github.com>
2018-12-21 15:04:13 -0300

    Avances
    mejora de login, avences de diseño, avances en las auditorias y eliminar

---
+++ MCReg/classapp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCReg
+{
+    public static class Classapp
+    {
+       public static string usuario;
+        
+    }
+}
